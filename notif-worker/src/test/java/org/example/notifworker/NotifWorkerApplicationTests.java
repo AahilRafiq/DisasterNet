5b82d7c6581ddc96a66684a2a66cb2c8533e7ea2
AahilRafiq <aahil.rafiq136@gmail.com>
2025-10-16 22:20:49 +0530

    init notif-worker module

---
+++ notif-worker/src/test/java/org/example/notifworker/NotifWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.notifworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
